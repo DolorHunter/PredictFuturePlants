1d8300155d71bb81bedf31c4c61eeefc64180782
DolorHunter <DolorHunter@gmail.com>
2019-06-16 17:12:25 +0800

    0616 - Creat backward & forward

---
+++ backward.py
@@ -0,0 +1,75 @@
+#coding:utf-8
+
+import tensorflow as tf
+import file
+import forward
+import os
+
+BATCH_SIZE = 1                # 定义每轮喂入神经网络多少张图片
+LEARNING_RATE_BASE = 1.00      # 学习率0.01
+LEARNING_RATE_DECAY = 0.0    # 衰减率0.99
+REGULARIZER = 0.01          # 正则化系数0.0001
+STEPS = file.Z1_IMAGE         # 训练轮数为图片个数
+MOVING_AVERAGE_DECAY = 0.99   # 滑动平均衰减率0.99
+MODEL_SAVE_PATH = "./model/"  # 模型保存路径
+MODEL_NAME = "model"          # 模型se保存文件名
+
+
+def backward():  # backward函数中读入图片数据
+    x = tf.placeholder(tf.float32, [None, forward.INPUT_NODE])  # 用placeholder给x占位
+    y_ = tf.placeholder(tf.float32, [None, forward.OUTPUT_NODE])  # 用placeholder给y_占位
+    y = forward.forward(x, REGULARIZER)  # 用前向传播函数计算输出y
+    global_step = tf.Variable(0, trainable=False)  # 给轮数计数器global_step赋初值，定义为不可训练
+
+    ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.arg_max(y_, 1))
+    cem = tf.reduce_mean(ce)
+    loss = cem + tf.add_n(tf.get_collection('losses'))  # 调用包含正则化的损失函数loss
+
+    learning_rate = tf.train.exponential_decay(  # 定义指数衰减学习率
+        LEARNING_RATE_BASE,
+        global_step,
+        file.Z1_IMAGE / BATCH_SIZE,
+        LEARNING_RATE_DECAY,
+        staircase=True
+    )
+
+    train_step = tf.train.ProximalGradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+
+    ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    ema_op = ema.apply(tf.trainable_variables())
+    with tf.control_dependencies([train_step, ema_op]):
+        train_op = tf.no_op(name='train')
+
+    saver = tf.train.Saver()  # 实例化Saver
+
+    with tf.Session() as sess:  # 在with结构中初始化所有变量
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+
+        # 断点续训
+        ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH)
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+
+        for i in range(STEPS):  # 用for循环迭代steps轮
+            # 每次读入图片和序号，喂入神经网络进行训练
+            xs = file.arr_image_z1(STEPS)
+            ys = file.arr_image_z1(STEPS)
+            # xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            # 训练，得到损失和步骤，输入为xs, ys
+            _, loss_value, step = sess.run([train_op, loss, global_step],
+                                           feed_dict={x: xs, y_: ys})
+
+            print("After %d training steps, loss on training batch is %g" % (step, loss_value))
+            if i % 100 == 0:
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
+
+
+def main():
+    # mnist = input_data.read_data_sets("./data/", one_hot=True)
+    arr = file.input_data_z1()
+    backward()
+
+
+if __name__ == '__main__':
+    main()
--- file.py
+++ file.py
@@ -5,10 +5,12 @@ from PIL import Image
 
 ROW_SIZE = 1200
 COL_SIZE = 1200
+Z1_LOC = '.\\data\\Z1\\Z1-'
+Z1_IMAGE = 212
 
 
 # Convert image to array[1*1440000] in float32
-def arr_image(num):
+def arr_image_z1(num):
     if num >= 1000:
         print("Too many image! Maximum scale is 999.")
     else:
@@ -16,20 +18,22 @@ def arr_image(num):
         tens = int((num / 10) % 10)
         hundreds = int((num / 100) % 10)
         str_num = chr(hundreds + 48) + chr(tens + 48) + chr(units + 48)
-        im = Image.open('.\data\Z1\Z1-' + str_num + '.tif')
+        im = Image.open(Z1_LOC + str_num + '.tif')
         # im = im.resize((28, 28), Image.ANTIALIAS)
         im_arr = np.array(im.convert('L'))  # Convert to array
         nm_arr = im_arr.reshape([1, 1440000])
-        nm_arr = nm_arr.astype(np.int16)
+        nm_arr = nm_arr.astype(np.float32)
         arr_ready = np.multiply(nm_arr, 1/255)
         return arr_ready
 
 
+def input_data_z1():
+    for i in range(1, Z1_IMAGE+1):
+        arr = arr_image_z1(i)
+
+
 def main():
-    image_number = 5
-    for i in range(1, image_number+1):
-        print(i)
-        arr_image(i)
+    input_data_z1()
     return 0
 
 
